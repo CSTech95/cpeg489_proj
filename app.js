f7d53018133c8c6be04475e4458a48b89147bb43
Aser Docteur <aser@Asers-MacBook-Pro.local>
2022-10-18 02:04:18 -0400

    Add hikes route and template.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("/hikes", hike.index);
 app.post("/add_hike", hike.add_hike);
