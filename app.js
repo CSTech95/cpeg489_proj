e3b25af7f367c9bd08a10210cbd09bd78dca6388
Aser Docteur <aser@Asers-MacBook-Pro.local>
2022-10-23 20:02:57 -0400

    Fixes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var hike = require("./routes/hike");
+//var hike = require("./routes/hike");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//var indexRouter = require("./routes/index");
+//var usersRouter = require("./routes/users");
 
 var app = express();
 // TODO add routes, configure db, connect to front end
@@ -19,22 +19,25 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, "public")));
-//app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("/hikes", hike.index);
-app.post("/add_hike", hike.add_hike);
 
-app.get("/", (req, res) => {
-	res.send("request sent");
-	//res.json();
+app.use(express.static(path.join(__dirname, "/client/", "build")));
+app.use("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/views", "index.html"));
 });
-app.get("/flower", (req, res) => {
-	res.json({
-		name: "Tulip",
-		colour: "Blue-ish",
-	});
-	//console.log(res.json);
-});
+//app.get("/hikes", hike.index);
+//app.post("/add_hike", hike.add_hike);
+
+//app.get("/", (req, res) => {
+//	res.send("request sent");
+//	//res.json();
+//});
+//app.get("/flower", (req, res) => {
+//	res.json({
+//		name: "Tulip",
+//		colour: "Blue-ish",
+//	});
+//console.log(res.json);
+//});
 
 // catch 404 and forward to error handler
 //app.use(function (req, res, next) {
